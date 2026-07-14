01e3d5a6e9858862cf7b5fed7b9e36cf60b263e9
provariualex <provariualex@gmail.com>
2026-07-14 17:08:38 +0300

    of and from - create rxjs observables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('rxjs-project');
+  constructor() {
+    /** OF create an observable and is sending only once the full array */
+    const numbers$ = of([1, 2, 3, 4, 5]);
+    /** From create an observable and is sending every value from array one by one => stream of data */
+    const otherNumbers$ = from([1, 2, 3, 4, 5]);
+
+    numbers$.subscribe((data) => {
+      console.log(data);
+    });
+
+    otherNumbers$.subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
