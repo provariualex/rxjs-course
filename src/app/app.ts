5697aad1a49776649da3fc5bf464668c03150375
provariualex <provariualex@gmail.com>
2026-07-15 19:18:53 +0300

    error handling on observable and observer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 interface Comment {
@@ -18,7 +18,25 @@ interface Comment {
 })
 export class App {
   http = inject(HttpClient);
-  public comments$: Observable<Comment[]> = this.http.get<Comment[]>(
-    'http://localhost:3004/comments',
-  );
+  // public comments! : Comment[]
+  //
+  // constructor() {
+  //   this.http.get<Comment[]>('http://localhost:3004/commentss').subscribe({
+  //     next: comments => this.comments = comments,
+  //     error: err => this.comments = []
+  //   });
+  // }
+
+  comments$: Observable<Comment[]> = this.http
+    .get<Comment[]>('http://localhost:3004/commentss')
+    .pipe(
+      catchError(() => {
+        return of([]);
+      }),
+    );
 }
+
+/**
+ * something$ is an observable therefore we cannot call the next,error complete functions
+ * something$.subscribe(here is an observer inside an observable, here we can call the next, error, complete functions)
+ * */
