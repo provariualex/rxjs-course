b122f35fad21f972964ed4e108af70d8e1cb6081
provariualex <provariualex@gmail.com>
2026-07-15 16:47:32 +0300

    pipe(filter(),map()).subscribe() operators

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,21 +12,22 @@ export class App {
   constructor() {
     const users = [
       { id: '1', name: 'John', age: 25, isActive: true },
-      { id: '2', name: 'Dan', age: 125, isActive: false },
+      { id: '2', name: 'Dan', age: 125, isActive: true },
       { id: '3', name: 'Mike', age: 45, isActive: true },
     ];
     const users$: Observable<any> = new Observable((observer) => {
       observer.next(users);
     });
 
-    const filteredUsers$ = users$.pipe(
-      filter((users) => {
-        return users.every((user: { name: any }) => user.name);
-      }),
-    );
-
-    filteredUsers$.subscribe((data) => {
-      console.log(data);
-    });
+    const filteredUsers$ = users$
+      .pipe(
+        filter((users) => {
+          return users.every((user: { isActive: any }) => user.isActive);
+        }),
+        map((users) => {
+          return users.map((user: { name: any }) => user?.name + ' Customer');
+        }),
+      )
+      .subscribe((data) => console.log(data));
   }
 }
