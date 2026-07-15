5b546363296563decff5d6c3e71f111da3bd92ca
provariualex <provariualex@gmail.com>
2026-07-15 16:01:25 +0300

    filter operator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { map, of } from 'rxjs';
+import { filter, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +10,23 @@ import { map, of } from 'rxjs';
 })
 export class App {
   constructor() {
-    const user = [
-      { id: '1', name: 'John', age: 25 },
-      { id: '2', name: 'Dan', age: 125 },
-      { id: '3', name: 'Mike', age: 45 },
+    const users = [
+      { id: '1', name: 'John', age: 25, isActive: true },
+      { id: '2', name: 'Dan', age: 125, isActive: false },
+      { id: '3', name: 'Mike', age: 45, isActive: true },
     ];
-    const users$ = of(user).pipe(
-      map((users) => {
-        return users.map((user) => user.name);
+    const users$: Observable<any> = new Observable((observer) => {
+      observer.next(users);
+    });
+
+    const filteredUsers$ = users$.pipe(
+      filter((users) => {
+        return users.every((user: { name: any }) => user.name);
       }),
     );
-    users$.subscribe((data) => console.log(data));
+
+    filteredUsers$.subscribe((data) => {
+      console.log(data);
+    });
   }
 }
-
-/** there are 2 maps, one is the rxjs operator and other is the javascript map,
- *  because you get an array of users and want to map each user,
- *  if ou just wanted to return something else like an empty array,
- *  the second map would not be necessary. */
