c69d23a44720378de76afe25a06b78647c8e4149
provariualex <provariualex@gmail.com>
2026-07-16 12:39:23 +0300

    combineLatest() - will combine 2 streams into one

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, combineLatest, from, Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 interface Comment {
@@ -16,4 +16,29 @@ interface Comment {
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  public users = [
+    { id: '1', name: 'Dan' },
+    { id: '2', name: 'Ana' },
+    { id: '3', name: 'Mihai' },
+  ];
+
+  messagePromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('promise resolved');
+    }, 5000);
+  });
+
+  data$ = combineLatest({
+    users: of(this.users),
+    messagePromise: from(this.messagePromise),
+  });
+
+  constructor() {
+    this.data$.subscribe((result) => console.log(result));
+  }
+}
+
+/** combineLatest() will wait for both streams to send at least one value and only after that will emit the combined observables,
+ * if one of the observables doesent emit at elast once then you will never get any values.
+ */
