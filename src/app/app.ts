730bcff33d88a0484dd1252d8e64e6e94e47dd92
provariualex <provariualex@gmail.com>
2026-07-17 14:32:59 +0300

    distinct() operator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { catchError, combineLatest, forkJoin, from, Observable, of, withLatestFrom } from 'rxjs';
+import {
+  catchError,
+  combineLatest,
+  distinct,
+  forkJoin,
+  from,
+  Observable,
+  of,
+  withLatestFrom,
+} from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 interface Comment {
@@ -17,21 +26,20 @@ interface Comment {
   styleUrl: './app.scss',
 })
 export class App {
-  http = inject(HttpClient);
+  public users = [
+    { id: '1', name: 'Dan', age: 25, isActive: true },
+    { id: '2', name: 'Ana', age: 25, isActive: true },
+    { id: '3', name: 'Dana', age: 25, isActive: true },
+    { id: '4', name: 'Emma', age: 3.5, isActive: true },
+  ];
 
   constructor() {
-    const comments$ = this.http.get('http://localhost:3004/comments');
-
-    const customValue$ = new Observable((observer) => {
-      observer.next('initial value');
-      setTimeout(() => {
-        observer.next('second value');
-      }, 5000);
-    });
-
-    comments$.pipe(withLatestFrom(customValue$)).subscribe((result) => console.log(result));
+    const user$ = from(this.users)
+      .pipe(distinct((user) => user.age === 25))
+      .subscribe((user) => console.log(user));
   }
 }
 
-/** withLatestFrom will just track the comments stream and take whatever the custom value ahs at that particular time,
- *  if the customValue$ will update this stream will ignore  */
+/** distinct() will emit only distinct values, in this case only first user that has age 25 will be emitted,
+ *  after that other users that have same age will be skipped,
+ * but if user has a different age will be emitted  */
